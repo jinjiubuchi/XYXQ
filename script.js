3d0dbb3b96033e8762cea5c440d6bcbbfc34b5eb
jinjiubuchi <342217767@qq.com>
2024-11-30 23:21:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-// 添加一些简单的互动效果，比如按钮点击提示
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('click', function() {
-        alert('你点击了一个按钮！');
-    });
+// 获取按钮元素
+const learnMoreBtn = document.getElementById('learnMoreBtn');
+const showMoreCoursesBtn = document.getElementById('showMoreCoursesBtn');
+const gameBtn = document.getElementById('gameBtn');
+const parentResourceBtn = document.getElementById('parentResourceBtn');
+
+// 设置按钮点击事件
+learnMoreBtn.addEventListener('click', () => {
+    alert('这里可以跳转到课程详情页面！');
+});
+
+showMoreCoursesBtn.addEventListener('click', () => {
+    alert('这里可以展示更多课程内容！');
+});
+
+gameBtn.addEventListener('click', () => {
+    alert('进入游戏乐园！');
+});
+
+parentResourceBtn.addEventListener('click', () => {
+    alert('家长专区资源内容！');
 });
